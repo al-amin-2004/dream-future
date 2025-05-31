bb4253aa04d1896f3076381f0485476c954d9481
Al amin <alaminmridha2004@gmail.com>
2025-06-01 00:20:13 +0600

    profile page update with balance section add

--- src/app/profile/[slug]/@profile/page.js
+++ src/app/profile/[slug]/@profile/page.js
@@ -46,8 +46,27 @@ const Profile = async ({ params }) => {
             </div>
 
 
-            <div className=" w-full flex flex-col h-full gap-10">
-                <div className="bg-amber-600 h-full rounded-2xl">2</div>
+            <div className="w-full flex flex-col h-full gap-10">
+
+                <div className="bg-white h-full rounded-xl p-2 md:p-5">
+                    <div>
+                        <p className="text-sm md:text-xl">Total Balance</p>
+                        <p className="text-xl md:text-3xl font-medium md:font-bold">{user.totalMoney}.00 BDT</p>
+                    </div>
+
+                    <div className="flex justify-between md:justify-around text-center bg-slate-400 py-1.5 md:py-3 px-2.5 rounded-2xl mt-3 md:mt-28">
+                        <div className="space-y-1.5">
+                            <p className="bg-orange-400 size-11 mx-auto p-1.5 rounded-full font-medium md:text-xl">{user.totalDeposit}</p>
+                            <p className="text-sm md:text-base">Total Diposit</p>
+                        </div>
+                        <div className="space-y-1.5">
+                            <p className="bg-orange-400 size-11 mx-auto p-1.5 rounded-full font-medium md:text-xl">{user.totalExtra}</p>
+                            <p className="text-sm md:text-base">Extra Diposit</p>
+                        </div>
+                    </div>
+                </div>
+
+
                 <div className="bg-amber-600 h-full rounded-2xl">3</div>
             </div>
         </div>
